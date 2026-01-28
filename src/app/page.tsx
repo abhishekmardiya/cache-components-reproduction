67d83ee09b0e670c88e5e55d67eb35735cf138f0
Abhishek Mardiya <mardiyaabhishek@gmail.com>
2026-01-28 21:30:05 +0530

    Update links in Home component to open in new tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,19 @@ export default function Home() {
   return (
     <main className="flex h-screen w-screen items-center justify-center">
       <div className="flex flex-col gap-4">
-        <Link href="/test-1/abc" className="text-2xl font-bold underline">
+        <Link
+          href="/test-1/abc"
+          className="text-2xl font-bold underline"
+          target="_blank"
+        >
           Go to Test 1 Route
         </Link>
         <br />
-        <Link href="/test-2/abc" className="text-2xl font-bold underline">
+        <Link
+          href="/test-2/abc"
+          className="text-2xl font-bold underline"
+          target="_blank"
+        >
           Go to Test 2 Route
         </Link>
       </div>
